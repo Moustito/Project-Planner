07725d8afb057761cfc555b015e65950a9096850
Titouan Moussiaux <moussiaux.titouan@gmail.com>
2022-08-10 14:36:06 +0200

    js- New task

---
+++ assets/js/new_task.js
@@ -0,0 +1,32 @@
+const taskList = [];
+let createTask = document.getElementById("createTask");
+
+createTask.addEventListener("click", (event) => {
+  console.log("click");
+  pushNewTask();
+});
+
+function pushNewTask() {
+  // title
+  const obj = [];
+  let inputTitle = document.getElementById("title").value;
+  obj["title"] = inputTitle;
+  // date
+  let inputDate = document.getElementById("date").value;
+  obj["date"] = inputDate;
+  // start time
+  let inputStartTime = document.getElementById("startTime").value;
+  obj["startTime"] = inputStartTime;
+  // end time
+  let inputEndTime = document.getElementById("endTime").value;
+  obj["endTime"] = inputEndTime;
+  // desciption
+  let inputDescription = document.getElementById("description").value;
+  obj["description"] = inputDescription;
+
+  taskList.push(obj);
+
+  location.href = "index.html";
+
+  console.log(taskList);
+}
